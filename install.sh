51fdfe9db6328d42532117777858e9587516744b
sjntn <s.jinda0619@gmail.com>
2015-10-12 11:28:55 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,19 @@
+#!/bin/sh
+
+# make symbolic links
+for name in home/*; do
+  target=$HOME/.`basename $name`
+  if [ -e "$target" ]; then
+    if [ ! -L "$target" ]; then
+      echo "WARNING: $target exists but is not a symlink."
+    fi
+  else
+    ln -s "$PWD/$name" "$target"
+  fi
+done
+
 # Install Homebrew
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+which brew > /dev/null
+if [ "$?" -ne 0 ]; then
+  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
