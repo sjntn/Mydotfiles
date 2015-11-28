93d93614a8fcc5a0c258a6c62d65dfe897883662
Shoya Jinda <s.jinda0619@gmail.com>
2015-11-28 15:18:56 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,12 +18,39 @@ if [ "$?" -ne 0 ]; then
   ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
 
-# Install vim color scheme
-mkdir -p ~/.vim/colors/
-cp ./vim_color/railscasts.vim ~/.vim/colors/
- 
 # Install NeoBundle
 if [ ! -d $HOME/.vim/bundle/neobundle.vim ]
 then
   curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh | sh
+fi
+
+# Install software and apps
+brew brewdle
+
+# Show hidden files with Finder
+read -p "Do you wish to show hidden files with Finder? y/n " yn
+if [ $yn = "y" -o $yn = "Y" ]; then
+    defaults write com.apple.finder AppleShowAllFiles TRUE
+    echo "done!!"
+    echo "...restart finder\n"
+    killall Finder
+fi
+
+# Never create .DS_Store
+read -p "Never create .DS_Store? y/n" yn
+if [ $yn = "y" -o $yn = "Y" ]; then
+    defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+    echo "...success"
+fi
+
+# install OhMyZsh
+if [ ! -d $HOME/.oh-my-zsh ]; then
+  curl -L http://install.ohmyz.sh | sh
+fi
+
+# Make default shell
+read -p "Do you wish to change default shell? zsh y/n " yn
+if [ $yn = "y" -o $yn = "Y" ]; then
+    chsh -s /bin/zsh
+    echo "done!!"
 fi
